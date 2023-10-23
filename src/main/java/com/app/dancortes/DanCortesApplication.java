7a1bdccdc5e0e81c5285f8908f175dd2a09a3d2e
Guilherme Mendes <gui.zz02@hotmail.com>
2023-10-23 20:00:42 -0300

    first

---
+++ src/main/java/com/app/dancortes/DanCortesApplication.java
@@ -0,0 +1,13 @@
+package com.app.dancortes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanCortesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanCortesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/dancortes/DanCortesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dancortes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanCortesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
